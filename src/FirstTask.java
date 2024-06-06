d72a89280c7acc84d3d488af78e5cdc6a0909841
kiril <kirillitsuckerman@gmail.com>
2024-06-06 21:50:59 +0300

    Initial commit

---
+++ src/FirstTask.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class FirstTask {
+    public static void main(String [] args) {
+        Scanner in = new Scanner(System.in);
+        double fahrenheit = in.nextDouble();
+
+        double celsius = (fahrenheit - 32.0) * 5.0 / 9;
+        System.out.println(fahrenheit + "  градусов по Фаренгейту равна " + celsius + " по Цельсию");
+    }
+}
